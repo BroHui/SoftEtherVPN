b592762621fbf8d52f7795508a51b5c24d77f6ee
Hui <hui.dev@outlook.com>
2024-12-30 11:51:05 +0800

    Update Restricted

--- src/Cedar/Server.c
+++ src/Cedar/Server.c
@@ -10749,15 +10749,7 @@ void SiGetCurrentRegion(CEDAR *c, char *region, UINT region_size)
 // 
 bool SiIsEnterpriseFunctionsRestrictedOnOpenSource(CEDAR *c)
 {
-	char region[128];
-	bool ret = false;
-	// Validate arguments
-	if (c == NULL)
-	{
-		return false;
-	}
-
-	return ret;
+	return false;
 }
 
 // Update the current region
